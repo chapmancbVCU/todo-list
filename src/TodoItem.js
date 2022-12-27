0f473abcf2455c9135ffceb0a51e40389e60e132
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-26 21:48:50 -0500

    Initial commit

---
+++ src/TodoItem.js
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *         Name: index.js
+ *       Author: Chad Chapman
+ * Date Created: December 26, 2022
+ *  Description: Functions that support implementation of Todo List item
+******************************************************************************/
+
+export class TodoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
