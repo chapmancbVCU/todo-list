a644ddc9ca72b6729c5e3d48870d1e3393e9f878
Chad Chapman <chad.chapman2010@gmail.com>
2023-01-14 18:40:39 -0500

    Initial commit

---
+++ src/AddItem.js
@@ -0,0 +1,12 @@
+/******************************************************************************
+ *         Name: AddItem.js
+ *       Author: Chad Chapman
+ * Date Created: January 14, 2023
+ *  Description: Class that contains functions for rendering form for adding 
+ *               an item.  More specifically the modal div that presents a 
+ *               web form.
+******************************************************************************/
+
+class AddItem {
+    
+}
