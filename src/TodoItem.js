871036a113889dd2f7957012abaacb6147e9bbf1
Chad Chapman <chad.chapman2010@gmail.com>
2023-01-14 17:43:16 -0500

    Added function inside constructor for reporting info about todo item.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -11,5 +11,14 @@ export class TodoItem {
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+
+        /**
+         * Function inside constructor that reports information about todo
+         * item.
+         * @returns String containing information about todo item.
+         */
+        this.info = function() {
+            return `Title: ${this.title}; Description: ${this.description}; Due Date: ${this.dueDate}; Priority: ${this.priority}`;
+        }
     }
 }
