34df4f6ebde8278b5344f7922c99c9bf3798aaee
Chad Chapman <chad.chapman2010@gmail.com>
2023-01-16 13:47:44 -0500

    Added list of items you can add to modal.  Added title to modal and some styling.

--- src/AddItem.js
+++ src/AddItem.js
@@ -41,7 +41,16 @@ export class AddItem {
 
         const modalContent = document.createElement('div');
         modalContent.classList.add('modal-content');
-        modalContent.appendChild(this.renderCloseButton());
+
+        const modalTitleContainer = document.createElement('div');
+        modalTitleContainer.classList.add('modal-title-container');
+        const modalTitle = document.createElement('div');
+        modalTitle.classList.add('modal-title');
+        modalTitle.textContent = 'Add New Item';
+        modalTitleContainer.appendChild(modalTitle);
+        modalTitleContainer.appendChild(this.renderCloseButton());
+
+        modalContent.append(modalTitleContainer);
         
         const modalMain = document.createElement('div');
         modalMain.classList.add('modal-main');
@@ -73,7 +82,20 @@ export class AddItem {
         modalSideBar.setAttribute('id', 'modal-sidebar');
         modalSideBar.classList.add('modal-sidebar');
 
-        
+        const toDoPageLink = document.createElement('div');
+        toDoPageLink.setAttribute('id', 'todo-page-link');
+        toDoPageLink.textContent = 'Todo List';
+        modalSideBar.appendChild(toDoPageLink);
+
+        const projectsPageLink = document.createElement('div');
+        projectsPageLink.setAttribute('id', 'projects-page-link');
+        projectsPageLink.textContent = 'My Projects';
+        modalSideBar.appendChild(projectsPageLink);
+
+        const notesPageLink = document.createElement('div');
+        notesPageLink.setAttribute('id', 'notes-page-link');
+        notesPageLink.textContent = 'Notes';
+        modalSideBar.appendChild(notesPageLink);
 
         return modalSideBar;
     }
