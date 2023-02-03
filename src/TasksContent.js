950dba4c81cb7f2f08938ac1fa52cc16e406d5bd
Chad Chapman <chad.chapman2010@gmail.com>
2023-02-03 18:51:22 -0500

    Testing with parsing of deserialized object.

--- src/TasksContent.js
+++ src/TasksContent.js
@@ -1,3 +1,5 @@
+import { TodoItem } from "./TodoItem"; 
+
 export class TasksContent {
 
     constructor() {
@@ -7,10 +9,34 @@ export class TasksContent {
     renderTasks() {
         for(let i = 0; i < localStorage.length; i++) {
             const key = localStorage.key(i);
-           const value = localStorage.getItem(key);
-           const pElement = document.createElement('p');
-           pElement.textContent = value;
-           this.tasksContainer.appendChild(pElement);
+            
+            const deserializedObj = JSON.parse(localStorage.getItem(key));
+
+            const title = deserializedObj.title;
+            const description = deserializedObj.description;
+            const dueDate = deserializedObj.dueDate;
+            const priority = deserializedObj.priority;
+            
+            const todoItemContainer = document.createElement('div');
+            todoItemContainer.classList.add('todo-item');
+            
+            const titleContent = document.createElement('p');
+            titleContent.textContent = `Title: ${title}`;
+            todoItemContainer.appendChild(titleContent);
+
+            const descriptionContent = document.createElement('p');
+            descriptionContent.textContent = `Description: ${description}`;
+            todoItemContainer.appendChild(descriptionContent);
+
+            const dueDateContent = document.createElement('p');
+            dueDateContent.textContent = `Due by: ${dueDate}`;
+            todoItemContainer.appendChild(dueDateContent);
+
+            const priorityContent = document.createElement('p');
+            priorityContent.textContent = `Priority: ${priority}`;
+            todoItemContainer.appendChild(priorityContent);
+
+            this.tasksContainer.appendChild(todoItemContainer);
        }
     }
 }
