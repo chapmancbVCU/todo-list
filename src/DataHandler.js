fd699eeedb59a3335583cddbf1cb7ab08375d5c7
Chad Chapman <chad.chapman2010@gmail.com>
2023-02-03 19:39:04 -0500

    Added constructor and implementation of getTodoItem function.

--- src/DataHandler.js
+++ src/DataHandler.js
@@ -4,14 +4,32 @@
  * Date Created: February 2, 2023
  *  Description: Functions that support implementation of Todo List item
 ******************************************************************************/
-
+import { TodoItem } from "./TodoItem";
 
 export class DataHandler {
 
-   /*getTodoItem() {
-        let deserializedObj = JSON.parse(localStorage.getItem("todoTest"));
-        console.log(deserializedObj);
-    }*/
+    /**
+     * Default constructor for the DataHandler super class.
+     */
+    constructor() {
+
+    }
+
+    getTodoItem(key) {
+        let deserializedObj = JSON.parse(localStorage.getItem(key));
+        
+        const itemType = deserializedObj.itemType;
+        if(itemType == 'TodoItem') {
+            const parentProject = deserializedObj.parentProject;
+            const title = deserializedObj.title;
+            const description = deserializedObj.description;
+            const dueDate = deserializedObj.dueDate;
+            const priority = deserializedObj.priority;
+
+            const todoItem = new TodoItem(itemType, parentProject, title, description, description, priority);
+            return todoItem;
+        }
+    }
     
     setTodoItem(item, title) {
         let serializedObj = JSON.stringify(item);
