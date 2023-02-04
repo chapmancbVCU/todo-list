4e386eeea43080b33bc59541cb09ebcd87a94573
Chad Chapman <chad.chapman2010@gmail.com>
2023-02-04 15:04:19 -0500

    Created projects form

--- src/ProjectsForm.js
+++ src/ProjectsForm.js
@@ -14,7 +14,43 @@ export class ProjectsForm {
     initializeComponents() {
         const projectsFormContainer = document.createElement('div');
         projectsFormContainer.setAttribute('id', 'modal-form-container');
-        projectsFormContainer.textContent = 'Projects Form';
+
+        const projectsForm = document.createElement('form');
+        projectsForm.classList.add('modal-form');
+        projectsForm.setAttribute('method', 'get');
+        projectsForm.setAttribute('action', '#');
+
+        // Setup title
+        const titleRow = document.createElement('div');
+        titleRow.classList.add('form-row');
+        const projectsFormLabel = document.createElement('label');
+        projectsFormLabel.setAttribute('for', 'projects-title');
+        projectsFormLabel.textContent = 'Title:';
+        titleRow.appendChild(projectsFormLabel);
+        const title = document.createElement('input');
+        title.setAttribute('id', 'projects-title');
+        title.setAttribute('name', 'projects-title');
+        title.setAttribute('type', 'text');
+        title.setAttribute('minlength', '5');
+        title.setAttribute('maxlength', '50');
+        title.setAttribute('required', '');
+        title.setAttribute('placeholder', 'Ex: Workout checklist');
+        titleRow.appendChild(title);
+        projectsForm.appendChild(titleRow);
+
+        // Setup submit button
+        const buttonsRow = document.createElement('div');
+        buttonsRow.classList.add('buttons-form-row');
+        buttonsRow.classList.add('add-project-button-row');
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('id', 'add-project-button');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.classList.add('add-project-button');
+        submitButton.textContent = 'Add';
+        buttonsRow.appendChild(submitButton);
+
+        projectsForm.appendChild(buttonsRow);
+        projectsFormContainer.appendChild(projectsForm);
         return projectsFormContainer;
     }
 }
