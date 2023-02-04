758303203a34ab91a6bb47524da5daff9c591178
Chad Chapman <chad.chapman2010@gmail.com>
2023-02-04 17:01:43 -0500

    Added notes form to class.

--- src/NotesForm.js
+++ src/NotesForm.js
@@ -14,7 +14,38 @@ export class NotesForm {
     initializeComponents() {
         const notesFormContainer = document.createElement('div');
         notesFormContainer.setAttribute('id', 'modal-form-container');
-        notesFormContainer.textContent = 'Notes Form';
+
+        const notesForm = document.createElement('form');
+        notesForm.classList.add('modal-form');
+        notesForm.classList.add('notes-form');
+        notesForm.setAttribute('method', 'get');
+        notesForm.setAttribute('action', '#');
+
+        // Setup description textarea
+        const editorArea = document.createElement('tinymce-editor');
+        editorArea.setAttribute('id', 'notes-content');
+        editorArea.setAttribute('selector', 'notes-content');
+        editorArea.setAttribute('name', 'notes-content');
+        editorArea.setAttribute('plugins', 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount');
+        editorArea.setAttribute('toolbar', 'undo redo | | bold italic backcolor | strikethrough | bullist numlist | outdent indent | alignleft aligncenter alignright alignjustify | removeformat | help');
+        editorArea.setAttribute('menubar', 'false');
+        editorArea.setAttribute('height', '300');
+        editorArea.setAttribute('placeholder', 'Describe item here.');
+        notesForm.appendChild(editorArea);
+
+        // Setup submit button
+        const buttonsRow = document.createElement('div');
+        buttonsRow.classList.add('buttons-form-row');
+        buttonsRow.classList.add('add-note-button-row');
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('id', 'add-note-button');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.classList.add('add-note-button');
+        submitButton.textContent = 'Add';
+        buttonsRow.appendChild(submitButton);
+        notesForm.appendChild(buttonsRow);
+
+        notesFormContainer.appendChild(notesForm);
         return notesFormContainer;
     }
 }
