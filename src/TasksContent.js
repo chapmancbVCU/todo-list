d08126c226bb011d132eed59b2bbb6be35dcfbdd
Chad Chapman <chad.chapman2010@gmail.com>
2023-02-05 12:21:22 -0500

    Added function for just rendering todo items.  Added test in renderTasks function to check if item type is of TodoItem.

--- src/TasksContent.js
+++ src/TasksContent.js
@@ -26,36 +26,42 @@ export class TasksContent {
         for(let i = 0; i < localStorage.length; i++) {
             const key = localStorage.key(i);
             
-            let todoItem = new TodoItem();
-            todoItem = todoItem.getTodoItem(key);
-            const todoItemContainer = document.createElement('div');
-            todoItemContainer.classList.add('todo-item');
-            
-            const titleContent = document.createElement('p');
-            titleContent.textContent = `Title: ${todoItem.getTitle()}`;
-            todoItemContainer.appendChild(titleContent);
-
-            const descriptionContent = document.createElement('p');
-            descriptionContent.textContent = `Description: ${todoItem.getDescription()}`;
-            todoItemContainer.appendChild(descriptionContent);
-
-            const dueDateContent = document.createElement('p');
-            dueDateContent.textContent = `Due by: ${todoItem.getDueDate()}`;
-            todoItemContainer.appendChild(dueDateContent);
-
-            const priorityContent = document.createElement('p');
-            priorityContent.textContent = `Priority: ${todoItem.getPriority()}`;
-            todoItemContainer.appendChild(priorityContent);
-
-            const itemTypeContent = document.createElement('p');
-            itemTypeContent.textContent = `Item type: ${todoItem.getItemType()}`;
-            todoItemContainer.appendChild(itemTypeContent);
-
-            const parentProjectContent = document.createElement('p');
-            parentProjectContent.textContent = `Parent Project: ${todoItem.getParentProject()}`;
-            todoItemContainer.appendChild(parentProjectContent);
-
-            this.tasksContainer.appendChild(todoItemContainer);
+            if(key.includes('TodoItem')) {
+                this.renderTodoItem(key);
+            }
        }
+    }
+
+    renderTodoItem(key) {
+        let todoItem = new TodoItem();
+        todoItem = todoItem.getTodoItem(key);
+        const todoItemContainer = document.createElement('div');
+        todoItemContainer.classList.add('todo-item');
+        
+        const titleContent = document.createElement('p');
+        titleContent.textContent = `Title: ${todoItem.getTitle()}`;
+        todoItemContainer.appendChild(titleContent);
+
+        const descriptionContent = document.createElement('p');
+        descriptionContent.textContent = `Description: ${todoItem.getDescription()}`;
+        todoItemContainer.appendChild(descriptionContent);
+
+        const dueDateContent = document.createElement('p');
+        dueDateContent.textContent = `Due by: ${todoItem.getDueDate()}`;
+        todoItemContainer.appendChild(dueDateContent);
+
+        const priorityContent = document.createElement('p');
+        priorityContent.textContent = `Priority: ${todoItem.getPriority()}`;
+        todoItemContainer.appendChild(priorityContent);
+
+        const itemTypeContent = document.createElement('p');
+        itemTypeContent.textContent = `Item type: ${todoItem.getItemType()}`;
+        todoItemContainer.appendChild(itemTypeContent);
+
+        const parentProjectContent = document.createElement('p');
+        parentProjectContent.textContent = `Parent Project: ${todoItem.getParentProject()}`;
+        todoItemContainer.appendChild(parentProjectContent);
+
+        this.tasksContainer.appendChild(todoItemContainer);
     }
 }
