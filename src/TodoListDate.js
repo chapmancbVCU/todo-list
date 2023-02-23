005a697ba460d8404ead8a700126112db16e6ce0
Chad Chapman <chad.chapman2010@gmail.com>
2023-02-22 20:58:28 -0500

    Added similar function but accepts date object as a parameter.

--- src/TodoListDate.js
+++ src/TodoListDate.js
@@ -13,11 +13,26 @@ export class TodoListDate {
     }
 
     /**
-     * This function returns today's date as a string in yyyy-mm-dd format.
+     * This function returns today's date as a string in yyyy-mm-dd format and 
+     * respects local host's timezone.
      * @returns Today's date in yyyy-mm-dd format.
      */
     static getDate() {
         let date = new Date();
+        let year = date.toLocaleString("default", { year: "numeric" });
+        let month = date.toLocaleString("default", { month: "2-digit" });
+        let day = date.toLocaleString("default", { day: "2-digit" });
+        return `${year}-${month}-${day}`;
+    }
+
+    /**
+     * This function accepts a date object and returns the string as 
+     * yyyy-mm-dd format and respects local host's timezone.
+     * @param {Date} date The date object we want to process to get date as 
+     * string in the correct format.
+     * @returns Today's date in yyyy-mm-dd format.
+     */
+    static getDateFromString(date) {
         let year = date.toLocaleString("default", { year: "numeric" });
         let month = date.toLocaleString("default", { month: "2-digit" });
         let day = date.toLocaleString("default", { day: "2-digit" });
